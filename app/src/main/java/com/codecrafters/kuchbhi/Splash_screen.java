17f31baf1639b7f5ac51614d7bb8bcfc158d2c9a
amrender07 <theamrendersingh@gmail.com>
2023-10-14 22:03:16 +0530

    abcd

--- app/src/main/java/com/codecrafters/kuchbhi/Splash_screen.java
+++ app/src/main/java/com/codecrafters/kuchbhi/Splash_screen.java
@@ -2,13 +2,25 @@ package com.codecrafters.kuchbhi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Splash_screen extends AppCompatActivity {
-
+ImageView goto_signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        goto_signin=findViewById(R.id.goto_singin);
+        goto_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Splash_screen.this, Sign_in.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/codecrafters/kuchbhi/otp_verifation.java
@@ -0,0 +1,14 @@
+package com.codecrafters.kuchbhi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class otp_verifation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_verifation);
+    }
+}
